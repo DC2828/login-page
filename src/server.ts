95ae379a4a511e82875bfaf9ff9a49a9fe51b71b
DC2828 <mvkin2014@gmail.com>
2025-10-30 21:56:57 +0800

    Initial project commit

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import{ Request, Response } from 'express';
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(express.static('dist/scripts'));
+app.set('view engine', 'ejs');
+app.set("views", __dirname + "/../views");
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).render('index');
+});
+
+
+app.get('/login', (req: Request, res: Response) => {
+    res.status(200).render('index');
+});
+
+app.get('/menu', (req: Request, res: Response) => {
+    res.status(200).render('menu');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
